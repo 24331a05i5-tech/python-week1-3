63399f344b342f36e09bd5b958393eedbedd666b
24331a05i5-tech <24331a05i5@mvgrce.edu.in>
2025-12-17 20:53:00 +0530

    Add files via upload

---
+++ 3rd.py
@@ -0,0 +1,7 @@
+x=4
+y=27.8
+z=x+y
+print("Addition : ",z,type(z))
+print("Floating the Integer value x : ",float(x))
+print("Converting float to Integer value y : ",int(y))
+print("Boolean of x:",bool(x))
